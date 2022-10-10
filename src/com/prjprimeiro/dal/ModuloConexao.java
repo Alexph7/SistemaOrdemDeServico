ed40da54771ac15fe56a66bdbea624c166db3bbe
Alexph7 <alexph7.56@gmail.com>
2022-10-10 17:05:34 -0300

    alteração classe dal, nome de usuario, url e senha alterado para o novo usuario para usar ireport

--- src/com/prjprimeiro/dal/ModuloConexao.java
+++ src/com/prjprimeiro/dal/ModuloConexao.java
@@ -13,18 +13,18 @@ public class ModuloConexao {
         java.sql.Connection conexao = null;
         String driver = "com.mysql.cj.jdbc.Driver";
         //armazenando informações referentes ao banco
-        String url = "jdbc:mysql://localhost:3306/idealgaseagua";
-        String user = "root";
-        String password = "";
+        String url = "jdbc:mysql://localhost:3306/idealgaseagua?characterEncoding=utf-8";
+        String user = "dba";
+        String password = "1101";
 
         //Estabelecendo a conexão com o banco de dados.
          try {
             Class.forName(driver);
             conexao = DriverManager.getConnection(url, user, password);
             return conexao;
-        } catch (Exception e) {
+        } catch (ClassNotFoundException | SQLException e) {
             //A linha abaixo serve de apoio para esclarecer o erro
-            //System.out.println(e);
+           // System.out.println(e);
             return null;        }
          
     }
